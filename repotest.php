508e2bc2e9feaf415711a0cd1396e4af6123c9a7
Wettinger <gabriel-wettinger@gmx.de>
2011-07-18 18:58:08 +0200

    erster Commit

---
+++ repotest.php
@@ -0,0 +1,14 @@
+<?php
+$dir = "bilder/home/";
+
+$images = array();
+$d = dir($dir);
+
+while($name = $d->read())
+{
+    if(!preg_match('/\.(jpg|gif|png)$/', $name)) continue;	
+	echo '<img src="' . $dir.$name . '" height="450"  />';
+}
+
+$d->close();
+?>
